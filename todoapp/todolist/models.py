d03b2fa76a70d5db4b3efe0fb90097b6bcbbf702
bermet <bermet.alibek@gmail.com>
2023-07-31 22:12:16 -0500

    created model & registered in admin

--- todoapp/todolist/models.py
+++ todoapp/todolist/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class ToDo(models.Model):
+    title = models.CharField("Task", max_length=500)
+    is_complete = models.BooleanField("Complete", default=False)
+
+    class Meta:
+        verbose_name = "Task"
+        verbose_name_plural = "Tasks"
+
+    def __str__(self):
+        return self.title
+
