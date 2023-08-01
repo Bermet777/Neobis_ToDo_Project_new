f7ac3995952cce83dfb7f4b1badb0cf67d419443
bermet <bermet.alibek@gmail.com>
2023-07-31 22:39:41 -0500

    fixed typo in urlpatterns

--- todoapp/todoapp/urls.py
+++ todoapp/todoapp/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include('todolist.urls'))
+    path("", include('todolist.urls')),
 ]
